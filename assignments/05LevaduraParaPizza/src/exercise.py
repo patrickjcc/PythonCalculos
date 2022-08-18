b971027eaf869da6e5e1bc91699f7dc85b897618
patrickjcc <110926624+patrickjcc@users.noreply.github.com>
2022-08-18 09:59:18 -0500

    Update exercise.py

--- assignments/05LevaduraParaPizza/src/exercise.py
+++ assignments/05LevaduraParaPizza/src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
-
+    harina=float(input('Dame la harina en gramos: '))
+    
+    print('Necesitas estos gramos de levadura: '+str((harina*50)/1000))
 
 
 if __name__ == '__main__':
