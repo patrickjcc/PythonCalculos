bedef529da8166f2087751a7087c48de57693b8d
patrickjcc <110926624+patrickjcc@users.noreply.github.com>
2022-08-18 09:55:03 -0500

    Update exercise.py

--- assignments/01Peso/src/exercise.py
+++ assignments/01Peso/src/exercise.py
@@ -2,8 +2,8 @@ def main():
     #escribe tu código abajo de esta línea
     peso1=float(input('Dame el peso inicial: '))
     peso2=float(input('Dame el peso final: '))
-    tiempo=int(input('Dame el tiempo en meses: '))
-    print('Lo que debes bajar por mes es: +str(float(peso1-peso2)/tiempo)
-
+    tiempo=int(input('Dame la cantidad de meses: '))
+    
+    print('Lo que debes bajar por mes es: '+str(float((peso1-peso2) /tiempo)))
 if __name__ == '__main__':
     main()
