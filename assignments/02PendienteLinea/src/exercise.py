0b43cda39cef8482223a90c95b1bbf1c4010ee24
patrickjcc <110926624+patrickjcc@users.noreply.github.com>
2022-08-18 09:55:51 -0500

    Update exercise.py

--- assignments/02PendienteLinea/src/exercise.py
+++ assignments/02PendienteLinea/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-
+    X1=float(input('Dame x1: '))
+    Y1=float(input('Dame y1: '))
+    X2=float(input('Dame x2: '))
+    Y2=float(input('Dame y2: '))         
+    print('Pendiente: '+str((Y2-Y1)/(X2-X1)))
 
 
 if __name__ == '__main__':
